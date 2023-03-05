d10d683e3ba08e2d030854890db4f64c9a4cb418
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-05 17:40:12 +0200

    up

---
+++ FunctionsAndStatements/Functions&StatementsExercises/1.1 smalest- of-three.js
@@ -0,0 +1,1 @@
+const smallestOfThree = (n1,n2,n3) => Math.min(n1, n2 ,3)
--- FunctionsAndStatements/Functions&StatementsExercises/2.add-and-subtract.js
+++ FunctionsAndStatements/Functions&StatementsExercises/2.add-and-subtract.js
@@ -5,3 +5,11 @@ function sum(num1, num2, num3){
         return a-b
     }
 }
+//lector way
+function sum1(n1,n2,n3){
+    const add1 = (a, b) => a + b
+    const subtract1 = (a1, b1) => a1 - b1
+    console.log(subtract1(add1(n1, n2), n3))
+}
+sum1(3,5,2)
+sum(3,5,2)
