2a891595ad070778d7f269010b9a0542e337732e
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-11 10:24:32 +0200

    task3

---
+++ Objects&Classes/city_taxes.js
@@ -0,0 +1,17 @@
+function cityTaxes(name, population, treasury){
+    function city(city){
+        let cityEntries = Object.entries(city)
+       for (const [key, value] of cityEntries) {
+         console.log(`${key} -> ${value}`)
+         
+       }
+             
+     }
+     
+     
+}
+const city = 
+  cityTaxes('Tortuga',
+  7000,
+  15000);
+console.log(city);	
