839e01f1d2ad687f813717705d720773588468da
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-11 09:36:28 +0200

    task1,2

---
+++ Objects&Classes/city.js
@@ -0,0 +1,9 @@
+function city(object){
+   let cityEntries = Object.entries(object)
+  for (const [key, value] of cityEntries) {
+    console.log(`${key} -> ${value}`)
+    
+  }
+        
+}
+
---
+++ Objects&Classes/person-info.js
@@ -0,0 +1,21 @@
+function personInfo1(firstName, lastName, age){
+    let person = {
+        firstName,
+        lastName,
+        age
+    }
+
+
+
+
+    return person
+}
+function personInfo(firstName, lastName, age){
+    let person = {}
+    person.firstName = firstName
+    person.lastName = lastName
+    person.age = age
+
+
+    return person
+}
