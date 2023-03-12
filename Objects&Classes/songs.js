598d8b18e2d19ae74ca725b2d17d43f7a7509b58
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-12 10:33:51 +0200

    task10

---
+++ Objects&Classes/songs.js
@@ -0,0 +1,26 @@
+function solve(arr){
+    class Song {
+        constructor(type, name, time){
+            this.type = type
+            this.name = name
+            this.time = time
+        }
+    }
+    let songs = []
+    let numberOfSong = arr.shift()
+    let typeSong = arr.pop()
+
+    for (let i = 0; i < numberOfSong; i++){
+        let[type , name, time] = arr[i].split('_')
+        let song = new Song(type, name, time)
+        songs.push(song)
+    }
+    if (typeSong === 'all'){
+        songs.forEach((i) => console.log(i.name))
+    }
+    else{
+        let filtered = songs.filter((i) => i.type === typeSong)
+        filtered.forEach((i) => console.log(i.name))
+    }
+     
+}
