923c6eab768db5bb212b4bd422f8e296c76888d9
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-15 18:18:06 +0200

    up

--- Objects&Classes/Object&ClassesEx/inventory.js
+++ Objects&Classes/Object&ClassesEx/inventory.js
@@ -4,18 +4,19 @@ function inventory(input){
         let info = line.split(' / ')
         let [name, level, items] = [info[0], Number(info[1]), info[2] ]
         heroes.push({name , level, items})
+
         
     }
-    console.log(heroes[0])
-    console.log(heroes[1])
-    console.log(heroes[2])
-    
+    let sortedHeroes = heroes
+        .sort((a, b) => {
+            return a.level - b.level
+        })
+        
+    for (const {name, level, items} of sortedHeroes) {
+        console.log(`Hero: ${name}`)
+        console.log(`level => ${level}`)
+        console.log(`items => ${items}`)
+    }
 }
 
 
-inventory([
-    'Isacc / 25 / Apple, GravityGun',
-    'Derek / 12 / BarrelVest, DestructionSword',
-    'Hes / 1 / Desolator, Sentinel, Antara'
-    ])
-    
