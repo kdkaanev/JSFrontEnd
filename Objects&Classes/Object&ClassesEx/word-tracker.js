b8aeeb411f01a7796fc59b4cb4dc4e25314ce28a
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-15 19:51:53 +0200

    task6

---
+++ Objects&Classes/Object&ClassesEx/word-tracker.js
@@ -0,0 +1,21 @@
+function wordTracker(input){
+    let words = {}
+    let targetWords = input.shift().split(' ')
+    for (const word of targetWords) {
+        let count = input.filter((w) => w === word).length
+        words[word] = count
+        
+    }
+    let sortedWords = Object.entries(words)
+        .sort((word1, word2) => {
+        let [_name1, count1] = word1
+        let [_name2, count2] = word2
+
+        return count2 - count1
+    })
+    for (const [word, count] of sortedWords) {
+        console.log(`${word} - ${count}`)
+    }
+    
+   
+}   
