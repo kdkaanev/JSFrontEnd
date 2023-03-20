ac163bc27c3213390014a6fba718cbe7601722ab
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-20 19:46:33 +0200

    up task1

--- DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/01. Subtraction/subtract.js
+++ DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    const num1 = document.getElementById('firstNumber').disabled = false
+    const num2 = document.getElementById('secondNumber').disabled = false
+    let result = document.getElementById('result')
+
+    result = num1 - num2
+    console.log(result)
 }
