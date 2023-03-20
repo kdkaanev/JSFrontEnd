ad1c30bcd8e13b0eafd12a1639920f27c0985509
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-20 06:57:51 +0200

    up task5

--- DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/05. Delete-From-Table/app.js
+++ DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/05. Delete-From-Table/app.js
@@ -1,3 +1,15 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const result = document.getElementById('result')
+    const input = document.querySelector('body > label > input[type=text]')
+    const evenTd = Array.from(document.querySelectorAll('td:nth-child(2)'))
+    let emailValue = input.value
+    let foundElement = evenTd.find((td) => td.textContent === emailValue)
+    if(foundElement){
+        foundElement.parentElement.remove()
+        result.textContent = 'Deleted'
+        
+    }else{
+        result.textContent = 'Not found.'
+    }
+
 }
