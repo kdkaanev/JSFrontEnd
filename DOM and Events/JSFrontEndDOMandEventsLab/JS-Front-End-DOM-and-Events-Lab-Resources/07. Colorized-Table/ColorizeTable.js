e300bcf3cb5ac0ff8d91b27047658cafa1fac4eb
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-20 17:34:03 +0200

    up task7

--- DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/07. Colorized-Table/ColorizeTable.js
+++ DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/07. Colorized-Table/ColorizeTable.js
@@ -1,3 +1,19 @@
 function colorize() {
-    // TODO
+    function colorize() {
+        let evenSity = Array.from(document.querySelectorAll('tr:nth-child(even)'))
+        let btn = document.querySelector('button')
+        for (const element of evenSity) {
+            btn.addEventListener('click', colorized)
+    
+        function colorized(){
+            element.style.backgroundColor = 'teal'
+        }
+        
+        }
+        
+        
+        
+        
+    }
+    
 }
