7bfaa822e45d3d051678636c559d514b431ff713
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-20 18:13:38 +0200

    exercises

--- DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/08. Sum-Table/SumTable.js
+++ DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/08. Sum-Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
+    let sum = 0
+    const input = Array.from(document.querySelectorAll('td:nth-child(even)'))
+    input.pop()
+    let costs = input
+    for (const cost of costs.values()) {
+        sum += Number(cost)
+    }
+    console.log(sum)
 }
