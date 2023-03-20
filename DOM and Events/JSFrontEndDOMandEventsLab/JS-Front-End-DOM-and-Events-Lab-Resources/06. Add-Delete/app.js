752f5febc88eb7277bbcbc478858d0288b085555
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-20 07:39:46 +0200

    up task6

--- DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/06. Add-Delete/app.js
+++ DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/06. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    const text = document.getElementById("newItemText").value
+    let li = document.createElement("li")
+    if(text.length === 0) return
+    li.appendChild(document.createTextNode(text))
+    document.getElementById("items").appendChild(li)
+    document.getElementById("newItemText").value = ''
+    let remove = document.createElement('a')
+    let linkText = document.createTextNode('[Delete]')
+    remove.appendChild(linkText)
+    remove.href = '#'
+    remove.addEventListener('click', deleteItem)
+    li.appendChild(remove)
+    
+
+
+    function deleteItem(){
+        li.remove()
+    }
+
 }
