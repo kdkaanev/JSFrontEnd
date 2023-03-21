0d07aae0062e23013fffac4bde53340f9dfbe0ab
kdkaanev <kdkaanev@github.com>
2023-03-21 11:03:42 +0200

    task2 up

--- DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/02. Sections/script.js
+++ DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/02. Sections/script.js
@@ -1,3 +1,23 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.getElementById('content');
+   
+   for (const word of words) {
+      const newDiv = document.createElement('div')
+      const newParagraph = document.createElement('p')
+      newParagraph.textContent = word
+      newParagraph.style.display = 'none'
+
+      newDiv.addEventListener('click',clickEventHeandler)
+
+      function clickEventHeandler(event){
+         const div = event.currentTarget;
+         const p = div.children[0]
+         p.style.display = 'block'
+      }
+      // ataching to DOM Tree
+      newDiv.appendChild(newParagraph)
+      content.appendChild(newDiv)
+      
+   }
+   
 }
