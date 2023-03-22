cdac1f1daa463dcc8ee71e7c706504e7b05f4d83
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-22 19:18:45 +0200

    task3 up

--- DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/03. Accordion/accordion.js
+++ DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/03. Accordion/accordion.js
@@ -1,3 +1,25 @@
-function toggle() {
-    console.log('TODO:...');
-}
+
+    function toggle() {
+        const extraP = document.querySelector('#extra')
+        const button = document.getElementsByClassName('button')[0]
+        
+        button.addEventListener('click', action)
+    
+        function action(event){
+            let btn = event.currentTarget
+            if(btn.textContent === 'MORE'){
+                extraP.style.display = 'block'
+                btn.textContent = 'LESS'
+            }else{
+                extraP.style.display = 'none'
+                btn.textContent = 'MORE'
+            }
+            
+    
+        }
+        
+    
+        
+        
+    }
+
