2b8f6d8e7ccabf035cafeb6b3f59a0748f0c7144
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-22 17:51:57 +0200

    уп

--- DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/03. Accordion/accordion.js
+++ DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-    console.log('TODO:...');
+    const button = document.getElementsByClassName('button')[0]
+    const extra = document.getElementById('extra')
+    button.addEventListener('click', changeStatus)
+
+    function changeStatus(event){
+        if ((extra.style.display === 'none' || extra.style.display === '')) {
+            extra.style.display = 'block'
+        }else{
+            extra.style.display = 'none'
+        }
+        
+    }
+    console.log(button);
 }
