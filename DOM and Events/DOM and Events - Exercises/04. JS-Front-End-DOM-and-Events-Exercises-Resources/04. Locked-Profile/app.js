b3c1ac55dfdef2e221f50a92e778bd1717615848
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-22 19:02:46 +0200

    task4 up

--- DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/04. Locked-Profile/app.js
+++ DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/04. Locked-Profile/app.js
@@ -1,3 +1,30 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const btns = Array.from(document.getElementsByTagName('button'))
+    
+    btns
+        .forEach((btn) => {
+            btn.addEventListener('click', information)
+        })
+
+        function information(e){
+            const button = e.currentTarget
+            const currentProfile = button.parentElement
+            let children = Array.from(currentProfile.children)
+            let lockInput = children[4]
+            const additionalInformationDiv = children[9]
+            let input = document.get
+            if(lockInput.checked){
+                if(button.textContent === 'Show more'){
+                    additionalInformationDiv.style.display = 'block'
+                    button.textContent = 'Hide it'
+    
+                }else{
+                    additionalInformationDiv.style.display = 'none'
+                    button.textContent = 'Show more'
+                }
+            }
+          
+
+        }
+   
 }
