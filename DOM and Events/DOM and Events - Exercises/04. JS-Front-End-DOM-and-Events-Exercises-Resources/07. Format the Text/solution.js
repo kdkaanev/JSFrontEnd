fa313904437450a457d8ca77129790acc49db7a8
kdkaanev <kdkaanev@github.com>
2023-03-23 11:38:04 +0200

    task7 up

--- DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/07. Format the Text/solution.js
+++ DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/07. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input')
+  const output = document.getElementById('output')
+  let textInput = input.value.split('.')
+  textInput.pop()
+  while(textInput.length > 0){
+    let paragraph = textInput.splice(0, 3)
+    
+    console.log(paragraph)
+    console.log(textInput)
+    debugger
+  }
+  
 }
