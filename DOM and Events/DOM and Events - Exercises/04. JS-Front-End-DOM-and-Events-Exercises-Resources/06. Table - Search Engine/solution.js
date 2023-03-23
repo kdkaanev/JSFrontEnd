7905e5ca912b5d40d1c2aa08dd09655af57c2de4
kdkaanev <kdkaanev@github.com>
2023-03-23 09:16:48 +0200

    task6 up

--- DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/06. Table - Search Engine/solution.js
+++ DOM and Events/DOM and Events - Exercises/04. JS-Front-End-DOM-and-Events-Exercises-Resources/06. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
+   const input = document.getElementById('searchField')
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      const searchWord = input.value
+      const body = Array.from(document.querySelectorAll('tbody tr'))
+      for (const row of body) {
+         let trimRow = row.textContent.trim()
+         if(row.classList.contains('select')){
+            row.classList.remove('select')
+         }
+         if(trimRow.includes(searchWord)){
+            row.classList.add('select')
+         }
+         
+      }
+      input.value = ''
+      
+      
    }
 }
