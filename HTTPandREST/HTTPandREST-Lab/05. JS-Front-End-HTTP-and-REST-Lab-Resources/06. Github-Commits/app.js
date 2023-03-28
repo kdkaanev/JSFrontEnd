b160f58e8b2a7d26fb6223fa8fb214748d41f262
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-28 18:54:13 +0300

    task6 up

--- HTTPandREST/HTTPandREST-Lab/05. JS-Front-End-HTTP-and-REST-Lab-Resources/06. Github-Commits/app.js
+++ HTTPandREST/HTTPandREST-Lab/05. JS-Front-End-HTTP-and-REST-Lab-Resources/06. Github-Commits/app.js
@@ -1,4 +1,25 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const BASE_URL = 'https://api.github.com/repos/'
+    const username = document.getElementById("username")
+    const repo = document.getElementById("repo")
+    const commits = document.getElementById("commits")
+
+    const usernameValue = username.value
+    const repoValue = repo.value
+
+    fetch(`${BASE_URL}${usernameValue}/${repoValue}/commits`)
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach(({commit}) => {
+                const li = document.createElement('li')
+                li.textContent = `${commit.author.name}: ${commit.message}`
+                commits.appendChild(li)
+            });
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+
+    
 }
