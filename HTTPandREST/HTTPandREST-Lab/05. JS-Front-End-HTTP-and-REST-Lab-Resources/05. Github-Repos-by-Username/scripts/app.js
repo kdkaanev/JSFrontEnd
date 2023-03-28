49f3df7b730ef2002e719e3e989483c42c7fec90
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-28 19:56:09 +0300

    up

--- HTTPandREST/HTTPandREST-Lab/05. JS-Front-End-HTTP-and-REST-Lab-Resources/05. Github-Repos-by-Username/scripts/app.js
+++ HTTPandREST/HTTPandREST-Lab/05. JS-Front-End-HTTP-and-REST-Lab-Resources/05. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,26 @@
 function loadRepos() {
-	console.log("TODO...");
+	const BASE_URL = 'https://api.github.com/users/'
+	const username = document.getElementById("username")
+	const usernameValue = username.value
+	const reposUl = document.querySelector('#repos > ul')
+	
+	fetch(`${BASE_URL}${usernameValue}/repos`)
+		.then((res) => res.json())
+		.then((data) => {
+			data.forEach((reposes) => {
+				console.log(reposes)
+				const a = document.createElement('a')
+				const li = document.createElement('li')
+				a.href = `${BASE_URL}/${usernameValue}/${reposes}`
+				a.textContent = reposes.name
+				li.appendChild(a)
+				
+				
+
+			});
+		})
+
+	
+
+	console.log(repos);
 }
