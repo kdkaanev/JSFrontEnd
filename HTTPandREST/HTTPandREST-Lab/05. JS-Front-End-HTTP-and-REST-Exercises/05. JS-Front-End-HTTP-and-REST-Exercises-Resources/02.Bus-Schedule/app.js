ea89f09a7e09ba8d5a831680718340ee8090f25b
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-03-31 18:33:18 +0300

    task 2 up

--- HTTPandREST/HTTPandREST-Lab/05. JS-Front-End-HTTP-and-REST-Exercises/05. JS-Front-End-HTTP-and-REST-Exercises-Resources/02.Bus-Schedule/app.js
+++ HTTPandREST/HTTPandREST-Lab/05. JS-Front-End-HTTP-and-REST-Exercises/05. JS-Front-End-HTTP-and-REST-Exercises-Resources/02.Bus-Schedule/app.js
@@ -1,16 +1,45 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
+  const BASE_URL = "http://localhost:3030/jsonstore/bus/schedule/";
+  let id = "depot";
+  const info = document.querySelector("#info > span");
+  const departButton = document.getElementById("depart");
+  const arriveButton = document.getElementById("arrive");
+  let stop = "";
+  let stopName = "";
 
-    async function arrive() {
-        // TODO:
-    }
+  function depart() {
+    fetch(`${BASE_URL}${id}`)
+      .then((res) => res.json())
+      .then((stops) => {
+        if (id === "depot") {
+          stop = "Depot";
+        } else {
+          stop = id;
+        }
+        stopName = stops.name;
+        id = stops.next;
+        info.textContent = `Next stop ${stopName}`;
 
-    return {
-        depart,
-        arrive
-    };
+        departButton.disabled = true;
+        arriveButton.disabled = false;
+      })
+      .catch((err) => {
+        info.textContent = "Error";
+        departButton.disabled = true;
+        arriveButton.disabled = true;
+      });
+  }
+
+  async function arrive() {
+    departButton.disabled = false;
+    arriveButton.disabled = true;
+    info.textContent = `Arriving at ${stopName}`;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
