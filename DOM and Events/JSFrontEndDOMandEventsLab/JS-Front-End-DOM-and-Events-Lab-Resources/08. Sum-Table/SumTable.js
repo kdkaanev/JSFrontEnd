77cb10083237ed4912e71d69e048b3e72861aed7
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-06-30 11:06:18 +0300

    task8 final

--- DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/08. Sum-Table/SumTable.js
+++ DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/08. Sum-Table/SumTable.js
@@ -1,6 +1,8 @@
 function sumTable() {
     let sum = 0
     const input = Array.from(document.querySelectorAll('body > table > tbody > tr > td:nth-child(2)'))
-    console.log(input)
+    input.forEach(x => sum += Number(x.textContent))
+    document.getElementById('sum').textContent = sum
+    console.log(sum)
     
 }
