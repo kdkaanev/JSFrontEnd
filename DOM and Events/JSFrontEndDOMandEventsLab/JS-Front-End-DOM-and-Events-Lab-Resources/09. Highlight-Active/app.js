12d618cf7dfe67e8d704da3402b8511492c7d92f
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-02 13:18:08 +0300

    task9 final

--- DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/09. Highlight-Active/app.js
+++ DOM and Events/JSFrontEndDOMandEventsLab/JS-Front-End-DOM-and-Events-Lab-Resources/09. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
+    const inputElements = Array.from(document.querySelectorAll('input'));
+    inputElements.forEach(input => {
+        input.addEventListener('focus', onFocus);
+        input.addEventListener('blur', onBlur);
+    
+    }
+    )
+    function onFocus(event) {
+        event.target.parentNode.className = 'focused';
+    
+    }
+    function onBlur(event) {
+        event.target.parentNode.className = '';
+    
+    
+    }
 }
