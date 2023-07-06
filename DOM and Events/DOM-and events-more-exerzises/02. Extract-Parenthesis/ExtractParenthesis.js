729c4d21178c28b4f8a3777595087aca77e7a565
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-06 20:17:10 +0300

    up

--- DOM and Events/DOM-and events-more-exerzises/02. Extract-Parenthesis/ExtractParenthesis.js
+++ DOM and Events/DOM-and events-more-exerzises/02. Extract-Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
+  const text = document.getElementById(content).textContent
+  let extractList = [];
+  for (const char of text) {
+    if (char.match(/\((.*?)\)/gm)){
+        extractList.push(char)
+        console.log(extractList)
+    }
+  }
+  
 
 }
