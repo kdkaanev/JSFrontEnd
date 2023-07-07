99a8d4f2e6c9b14d6e9ed2f74aeec83e66680a8f
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-07 19:59:57 +0300

    up

--- DOM and Events/DOM-and events-more-exerzises/03. Mouse-Gradient/app.js
+++ DOM and Events/DOM-and events-more-exerzises/03. Mouse-Gradient/app.js
@@ -1,3 +1,12 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const result = document.getElementById('result')
+    const target = document.getElementById('gradient')
+    target.addEventListener('mousemove', hoovrHandler)
+
+    function hoovrHandler(e){
+        let xTarget = e.offsetX 
+        let width = target.offsetWidth
+        let x = Math.floor((xTarget / width) * 100)
+        result.textContent = x + '%'
+    }
 }
