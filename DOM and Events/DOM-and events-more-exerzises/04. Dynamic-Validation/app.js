b7ef691bab6a3528c59d3344993690057c5f6152
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-09 13:28:08 +0300

    task4 final

--- DOM and Events/DOM-and events-more-exerzises/04. Dynamic-Validation/app.js
+++ DOM and Events/DOM-and events-more-exerzises/04. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    const email = document.getElementById('email');
+    
+    const regex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+    
+    email.addEventListener('change', () => {
+        if (regex.test(email.value)) {
+            email.classList.remove('error');
+        
+        }else{
+            email.classList.add('error')
+        }
+    }
+    )
 }
+
