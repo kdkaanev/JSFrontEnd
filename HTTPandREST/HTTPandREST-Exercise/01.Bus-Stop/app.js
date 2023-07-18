390696f48bbc6c7ac3a6bed566aea46d90b39668
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-18 13:36:49 +0300

    Update app.js

--- HTTPandREST/HTTPandREST-Exercise/01.Bus-Stop/app.js
+++ HTTPandREST/HTTPandREST-Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,28 @@
 function getInfo() {
-    // TODO:
-}
+    const stopIdInput = document.getElementById('stopId')
+    const stopId = stopIdInput.value 
+    const stopName = document.getElementById('stopName')
+    const buss = document.getElementById('buses')
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/businfo/'
+    buss.innerHTML =''
+    fetch(`${BASE_URL}${stopId}`)
+        .then((res) => res.json())
+        .then((busesInfo) =>{
+           let {name, buses} = busesInfo
+           stopName.textContent = name
+           for (const busId in buses) {
+            const li = document.createElement('li')
+                li.textContent = `Bus ${busId} arrives in ${buses[busId]} minutes`
+                buss.appendChild(li)
+            }
+           }        
+            )
+
+        .catch((err) =>{
+            stopName.textContent = 'Error'
+        })
+        
+
+    
+
+        }
