d910bc8b2af8ac67b0d3db0cc8d405e71dfccd09
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-18 16:51:23 +0300

    Update app.js

--- HTTPandREST/HTTPandREST-Exercise/02.Bus-Schedule/app.js
+++ HTTPandREST/HTTPandREST-Exercise/02.Bus-Schedule/app.js
@@ -1,10 +1,38 @@
 function solve() {
+    const info = document.querySelector('#info > span')
+    const departBtn = document.getElementById('depart')
+    const arriveBtn = document.getElementById('arrive')
+    const BASE_URL ='http://localhost:3030/jsonstore/bus/schedule/'
+    let id = 'depot'
     function depart() {
-        // TODO: 
+        departBtn.addEventListener('click', departHandler())
+
+        function departHandler(){
+            fetch(`${BASE_URL}${id}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    info.textContent = data.name
+                    id = data.next
+                    departBtn.setAttribute('disabled',true)
+                    arriveBtn.removeAttribute('disabled')
+                    console.log(data)
+                })
+                .catch((err) => {
+                    info.textContent = 'Error'
+                    departBtn.setAttribute('disabled',true)
+                    arriveBtn.setAttribute('disabled',true)
+                   
+                })
+        }
     }
 
     async function arrive() {
-        // TODO:
+        const response = await fetch(`${BASE_URL}{id}`)
+        const dataArrive = await response.json()
+        console.log(dataArrive)
+        arriveBtn.setAttribute('disabled',true)
+        departBtn.removeAttribute('disabled')
+
     }
 
     return {
