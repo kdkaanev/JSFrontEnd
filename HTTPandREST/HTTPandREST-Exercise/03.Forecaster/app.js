29f5eccc7b8f76247426bd57aa8adfab0939e129
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-19 11:23:30 +0300

    Update app.js

--- HTTPandREST/HTTPandREST-Exercise/03.Forecaster/app.js
+++ HTTPandREST/HTTPandREST-Exercise/03.Forecaster/app.js
@@ -1,5 +1,37 @@
 function attachEvents() {
-    // TODO:
+    
+    const submitBtn = document.getElementById('submit')
+    const BASE_URL = 'http://localhost:3030/jsonstore/forecaster/locations'
+    let id = ''
+    let alowedLocations = []
+    
+    fetch(BASE_URL)
+        .then((res) => res.json())
+        .then((data) =>{
+            submitBtn.addEventListener('click', bthHandler)
+            function bthHandler(){
+                const enterLocation = document.getElementById('location').value
+                data.forEach(element => {
+                    console.log(element)
+                    if(element.name === enterLocation){
+                        id = element.code
+                    }
+
+                });
+                fetch(`${BASE_URL}/today/${id}`)
+                    .then((response) => response.json())
+                    .then((todayForecast) =>{
+                        console.log(todayForecast)
+                    })
+                
+               
+            }
+        })
+        
+   
+       
+    
+
 }
 
 attachEvents();
