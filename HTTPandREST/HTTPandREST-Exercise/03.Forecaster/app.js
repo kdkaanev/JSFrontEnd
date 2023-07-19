d28dfaa2a6080d68aae7097abee429628417cce4
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-19 16:51:53 +0300

    Update app.js

--- HTTPandREST/HTTPandREST-Exercise/03.Forecaster/app.js
+++ HTTPandREST/HTTPandREST-Exercise/03.Forecaster/app.js
@@ -21,6 +21,7 @@ function attachEvents() {
                 const enterLocation = document.getElementById('location').value
                 const forecastDiv = document.getElementById('forecast')
                 forecastDiv.style.display = 'block'
+                let divCurrent = document.getElementById('current')
                 data.forEach(element => {
                     if(element.name === enterLocation){
                         id = element.code
@@ -30,7 +31,19 @@ function attachEvents() {
                 fetch(`${BASE_URL}/today/${id}`)
                     .then((response) => response.json())
                     .then((todayForecast) =>{
-                       
+                        console.log(todayForecast)
+                       let divForecasts = document.createElement('div')
+                       divForecasts.classList.add('forecasts')
+                        divCurrent.appendChild(divForecasts)
+                        let spanSimbol = document.createElement('span')
+                        spanSimbol.classList.add('condition', 'symbol')
+                        divCurrent.appendChild(spanSimbol)
+                        for (const key in todayForecast) {
+                            console.log(key)
+                        }
+                        
+                        
+
                     })
                     
                 
