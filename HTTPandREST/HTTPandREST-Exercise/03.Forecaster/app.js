c1a142d964a79be581a3b30f3b36231d91642a4e
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-07-19 13:56:46 +0300

    up task3

--- HTTPandREST/HTTPandREST-Exercise/03.Forecaster/app.js
+++ HTTPandREST/HTTPandREST-Exercise/03.Forecaster/app.js
@@ -1,18 +1,27 @@
 function attachEvents() {
+    let weaterSimbol = {
+        'Sunny' :'&#x2600',
+        'Partly sunny':'&#x26C5',
+        'Overcast' :'&#x2601',		
+        'Rain':'&#x2614',
+        'Degrees':'&#176',
+
+    }
     
     const submitBtn = document.getElementById('submit')
-    const BASE_URL = 'http://localhost:3030/jsonstore/forecaster/locations'
+    const BASE_URL = 'http://localhost:3030/jsonstore/forecaster/'
     let id = ''
     let alowedLocations = []
     
-    fetch(BASE_URL)
+    fetch(`${BASE_URL}/locations`)
         .then((res) => res.json())
         .then((data) =>{
             submitBtn.addEventListener('click', bthHandler)
             function bthHandler(){
                 const enterLocation = document.getElementById('location').value
+                const forecastDiv = document.getElementById('forecast')
+                forecastDiv.style.display = 'block'
                 data.forEach(element => {
-                    console.log(element)
                     if(element.name === enterLocation){
                         id = element.code
                     }
@@ -21,8 +30,9 @@ function attachEvents() {
                 fetch(`${BASE_URL}/today/${id}`)
                     .then((response) => response.json())
                     .then((todayForecast) =>{
-                        console.log(todayForecast)
+                       
                     })
+                    
                 
                
             }
