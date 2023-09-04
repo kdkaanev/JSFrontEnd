c6abe5558cec89d1357b458b1849d4bf57b93fa7
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2023-09-04 11:48:53 +0300

    Add files via upload

---
+++ projects/test/app.js
@@ -0,0 +1,45 @@
+function solve(){
+
+    BASE_URL =  'https://randomuser.me/api/'
+    const imgDiv = document.getElementById('image')
+    const btn = document.getElementById('click')
+    const nameDoc = document.getElementById('name')
+    const cellNum = document.getElementById('cell')
+    btn.addEventListener('click',btnHandler)
+
+    function btnHandler(){
+        
+        nameDoc.innerHTML = ''
+        imgDiv.innerHTML = ''
+        fetch(BASE_URL)
+            .then((res) => res.json())
+            .then(data=>{
+            let info = data.results
+            
+                    
+        let picUrl = info[0].picture.large
+    
+        let img = document.createElement('img')
+        img.setAttribute('src', picUrl);
+        imgDiv.appendChild(img)
+        let infoName = info[0]   
+        console.log(infoName)
+        let {cell, name} = infoName
+        cellNum.textContent = 'Tel.: ' + cell
+        let {title, first, last} = name
+        let nameTable = document.createElement('div')
+        
+        nameTable.textContent = `${title} ${first} ${last}`
+        nameDoc.appendChild(nameTable)
+        
+
+    }
+        )
+                    
+    }
+   
+
+    
+}
+
+solve()
